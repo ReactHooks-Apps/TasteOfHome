c609b8f8389c31f0ccffb4841fab02fc94a0a5e8
FidaaYousef <fidaa.as@gmail.com>
2021-01-31 12:11:06 +0200

    setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import NavTabs from './components/header.js';
+import ClassNames from './components/footer.js';
+
+function App() {
+  return (
+    <div className="App">
+    <NavTabs/>
+    <ClassNames/>
+      </div>
+  );
+}
+
+export default App;
